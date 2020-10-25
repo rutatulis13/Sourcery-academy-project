647a03a4ddb492647fe0158feb9e99e53772d6ce
Simas Leonavicius <simas.leonavicius@devbridge.com>
2020-10-25 12:29:41 +0200

    Add Api call.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GetStartedList from "./features/getStarted/components";
 
 function App() {
-  const instructions = [
-    {
-      id: 1,
-      instruction: "setup nvm",
-    },
-    {
-      id: 2,
-      instruction:
-        "run 'nvm use' or just set the node version to the one in .nvmrc file",
-    },
-    {
-      id: 3,
-      instruction: "run 'npm install'",
-    },
-    {
-      id: 4,
-      instruction: "run 'npm install -g json-server'",
-    },
-    {
-      id: 5,
-      instruction: "run 'npm run start-server'",
-    },
-    {
-      id: 6,
-      instruction: "run 'npm run start'",
-    },
-  ];
+  const [instructions, setInstructions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3008/instructions")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setInstructions(result);
+        },
+        (error) => {
+          // handle error here
+        }
+      );
+  }, []);
 
   return (
     <div className="App">
@@ -45,7 +32,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <GetStartedList instructions={instructions} />
+      <GetStartedList key={instructions.length} instructions={instructions} />
     </div>
   );
 }
