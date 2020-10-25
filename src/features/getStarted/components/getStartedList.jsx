02d7ca67eaf703d5e7c5f9cb194b70f30b194155
Simas Leonavicius <simas.leonavicius@devbridge.com>
2020-10-25 11:10:46 +0200

    Add component styles.

--- src/features/getStarted/components/getStartedList.jsx
+++ src/features/getStarted/components/getStartedList.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import "./getStartedList.scss";
+
 function GetStartedList({ instructions }) {
   return (
-    <div>
-      <h2>Instructions:</h2>
-      <ul>
+    <div className="get-started-list">
+      <h2 className="get-started-list__title">Instructions:</h2>
+      <ol className="get-started-list__list">
         {instructions.map((instruction) => (
-          <li key={instruction.id}>{instruction.instruction}</li>
+          <li key={instruction.id} className="get-started-list__list-item">
+            {instruction.instruction}
+          </li>
         ))}
-      </ul>
+      </ol>
     </div>
   );
 }
@@ -17,7 +21,7 @@ function GetStartedList({ instructions }) {
 GetStartedList.propTypes = {
   instructions: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.string,
+      id: PropTypes.number,
       instruction: PropTypes.string,
     })
   ),
