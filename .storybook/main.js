a27370538c952bdb6d84e0874dc7d5102fbc53b6
Goda Plytaityte <goda.plytaityte@devbridge.com>
2020-10-30 11:53:47 +0200

    Rename styles.scss file

--- .storybook/main.js
+++ .storybook/main.js
@@ -28,7 +28,7 @@ module.exports = {
           loader: "sass-loader",
           options: {
             prependData: `
-              @import 'src/styles/styles.scss';
+              @import 'src/styles/index.scss';
             `,
           },
         },
