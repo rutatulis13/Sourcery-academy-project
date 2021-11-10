a52d53f8b3ed854fa1c59de3921c5adb9d183ace
martynas9 <3martynas@gmail.com>
2021-11-10 22:14:57 +0200

    moved colors to global file, fixed bem naming, added responsive design features

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -5,19 +5,21 @@ import { ReactComponent as ChevronRightSvg } from "assets/chevron-right.svg";
 const Breadcrumbs = () => {
   return (
     <div className="breadcrumbs">
-      <ul className="breadcrumbs__ul">
-        <li className="breadcrumbs__ul__li">
-          <a href="/">Dashboard</a>
-        </li>
-        <li className="breadcrumbs__ul__li">
-          <ChevronRightSvg />
-          <a href="/">Reservations</a>
-        </li>
-        <li className="breadcrumbs__ul__li">
-          <ChevronRightSvg />
-          <a href="/">Books</a>
-        </li>
-      </ul>
+      <div className="breadcrumbs__content">
+        <ul className="breadcrumbs-list">
+          <li className="breadcrumbs-list__li">
+            <a href="/">Dashboard</a>
+          </li>
+          <li className="breadcrumbs-list__li">
+            <ChevronRightSvg />
+            <a href="/">Reservations</a>
+          </li>
+          <li className="breadcrumbs-list__li">
+            <ChevronRightSvg />
+            <a href="/">Books</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
