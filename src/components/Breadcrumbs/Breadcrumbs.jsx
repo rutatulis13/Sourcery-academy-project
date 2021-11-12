6359b18c729c5088b6f617e90fc9c45ee47ad9b7
martynas9 <3martynas@gmail.com>
2021-11-12 20:39:08 +0200

    made breadcrumbs functional

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,31 +1,42 @@
 import React, { useEffect, useState } from "react";
 import "./Breadcrumbs.scss";
 import { ReactComponent as ChevronRightSvg } from "assets/chevron-right.svg";
-import { useLocation } from "react-router";
+import { Link, useLocation } from "react-router-dom";
 
 const Breadcrumbs = () => {
   const routerLocation = useLocation();
-  const [breadcrumbsArray, setBreadcrumbsArray] = useState(
-    routerLocation.pathname.split("/")
-  );
+  const [breadcrumbsArray, setBreadcrumbsArray] = useState([]);
 
   useEffect(() => {
-    setBreadcrumbsArray(routerLocation.pathname.split("/"));
+    let splittedPathNames = routerLocation.pathname.split("/");
+    let currentPath = "";
+    let _breadcrumbsArray = [];
+    for (const page of splittedPathNames) {
+      if (page) {
+        currentPath += "/" + page;
+        _breadcrumbsArray = [
+          ..._breadcrumbsArray,
+          { name: page, path: currentPath },
+        ];
+      }
+    }
+    setBreadcrumbsArray(_breadcrumbsArray);
   }, [routerLocation]);
 
   return (
     <div className="breadcrumbs">
       <div className="breadcrumbs__content">
-        <div>{JSON.stringify(routerLocation)}</div>
         <ul className="breadcrumbs-list">
           {breadcrumbsArray.map((v, i) => (
-            <li className="breadcrumbs-list__li" key={v}>
-              {i > 0 ? <ChevronRightSvg /> : ""}
-              <a href="/">{i + " " + v}</a>
+            <li className="breadcrumbs-list__li" key={v.name}>
+              {i > 0 && <ChevronRightSvg />}
+              <Link className="breadcrumbs-link" to={v.path}>
+                {v.name}
+              </Link>
             </li>
           ))}
         </ul>
-        <ul className="breadcrumbs-list">
+        {/* <ul className="breadcrumbs-list">
           <li className="breadcrumbs-list__li">
             <a href="/">Dashboard</a>
           </li>
@@ -37,7 +48,7 @@ const Breadcrumbs = () => {
             <ChevronRightSvg />
             <a href="/">Books</a>
           </li>
-        </ul>
+        </ul> */}
       </div>
     </div>
   );
