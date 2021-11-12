11b743deb57319479c0dd43e44043a5959a8da50
martynas9 <3martynas@gmail.com>
2021-11-12 18:12:04 +0200

    testing router for breadcrumbs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
 import React from "react";
 import Layout from "components/Layout/Layout";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+} from "react-router-dom";
 
 function App() {
-  return <Layout>Container content</Layout>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="reservations" element={<Reservations />} />
+          </Route>
+          <Route path="*" element={<p>404</p>} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
+
+const NavigationLinks = () => {
+  return (
+    <div>
+      <Link to="/dashboard">Dashboard</Link>
+      <br />
+      <Link to="/dashboard/reservations">reservations</Link>
+      <br />
+      <Link to="/dashboard/reservations/books">books</Link>
+    </div>
+  );
+};
+
+const Dashboard = () => (
+  <div>
+    <NavigationLinks />
+    <br />
+    <p>Dashboard page</p>
+    <Outlet />
+  </div>
+);
+
+const Reservations = () => (
+  <div>
+    <p>Reservations page</p>
+  </div>
+);
 
 export default App;
--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Breadcrumbs.scss";
 import { ReactComponent as ChevronRightSvg } from "assets/chevron-right.svg";
+import { useLocation } from "react-router";
 
 const Breadcrumbs = () => {
+  const routerLocation = useLocation();
+  const [breadcrumbsArray, setBreadcrumbsArray] = useState(
+    routerLocation.pathname.split("/")
+  );
+
+  useEffect(() => {
+    setBreadcrumbsArray(routerLocation.pathname.split("/"));
+  }, [routerLocation]);
+
   return (
     <div className="breadcrumbs">
       <div className="breadcrumbs__content">
+        <div>{JSON.stringify(routerLocation)}</div>
+        <ul className="breadcrumbs-list">
+          {breadcrumbsArray.map((v, i) => (
+            <li className="breadcrumbs-list__li" key={v}>
+              {i > 0 ? <ChevronRightSvg /> : ""}
+              <a href="/">{i + " " + v}</a>
+            </li>
+          ))}
+        </ul>
         <ul className="breadcrumbs-list">
           <li className="breadcrumbs-list__li">
             <a href="/">Dashboard</a>
