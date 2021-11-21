0a4c9f5e1eafa8a0c614afe7fe184560005c1dd7
martynas9 <3martynas@gmail.com>
2021-11-22 00:00:46 +0200

    created grid component

---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Grid.scss";
+
+const Grid = (props) => {
+  return (
+    <div
+      className="grid"
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(auto-fill, minmax(min(${props.colWidth}, 100%), 1fr))`,
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+Grid.propTypes = {
+  children: PropTypes.node,
+  colWidth: PropTypes.string.isRequired,
+};
+
+export default Grid;
