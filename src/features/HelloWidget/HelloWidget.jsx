12a954b81440517e42a9af6a46842ba1e105320d
godagin <goda.gintalaite@gmail.com>
2021-11-22 17:09:13 +0200

    fix props in HelloWidget.jsx

--- src/features/HelloWidget/HelloWidget.jsx
+++ src/features/HelloWidget/HelloWidget.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Clock from "./Clock";
 import Greeting from "./Greeting";
 
 import "./HelloWidget.scss";
 
-const HelloWidget = (username = "User") => {
+const HelloWidget = ({ username = "User" }) => {
   return (
     <div className="hello-widget">
       <Clock />
       <Greeting name={username} />
     </div>
   );
+};
+HelloWidget.propTypes = {
+  username: PropTypes.string,
 };
 
 export default HelloWidget;
