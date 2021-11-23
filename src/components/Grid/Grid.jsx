b5e6d176922408abf263ae924c1c57140ae6f675
martynas9 <3martynas@gmail.com>
2021-11-23 23:03:06 +0200

    changed colWidth prop to breakpointCols array prop.

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -2,22 +2,41 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Grid.scss";
 
-const Grid = (props) => {
+const Grid = ({ children, breakpointCols }) => {
   return (
     <div
-      className="grid"
-      style={{
-        gridTemplateColumns: `repeat(auto-fill, minmax(min(${props.colWidth}, 100%), 1fr))`,
-      }}
+      className={`
+        grid
+        grid--mobile-${breakpointCols[0]}
+        grid--tablet-${breakpointCols[1]}
+        grid--laptop-${breakpointCols[2]}
+        grid--desktop-${breakpointCols[3]}
+      `}
+      // style={{gridTemplateColumns: `repeat(auto-fill, minmax(min(${props.colWidth}, 100%), 1fr))`,}}
     >
-      {props.children}
+      {children}
     </div>
   );
 };
 
 Grid.propTypes = {
+  breakpointCols: ({ breakpointCols: prop }, propName, componentName) => {
+    if (
+      !Array.isArray(prop) ||
+      prop.length !== 4 ||
+      !prop.every(Number.isInteger)
+    ) {
+      return new Error(
+        `Invalid prop ${propName} supplied to ${componentName}. It should be be an array of 4 numbers.`
+      );
+    }
+  },
   children: PropTypes.node,
-  colWidth: PropTypes.string.isRequired,
+  //colWidth: PropTypes.string,
+};
+
+Grid.defaultProps = {
+  breakpointCols: [1, 2, 3, 4],
 };
 
 export default Grid;
