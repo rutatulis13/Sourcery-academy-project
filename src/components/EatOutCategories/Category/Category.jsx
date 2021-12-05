51cdeab9f69141e9e601e47dc696eddcc5e669f8
Ruta Zuke <zuke.ruta@gmail.com>
2021-12-05 21:56:26 +0200

    fetch categories  data from api

--- src/components/EatOutCategories/Category/Category.jsx
+++ src/components/EatOutCategories/Category/Category.jsx
@@ -1,58 +1,103 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./Category.scss";
+import Grid from "components/Grid/Grid";
 
-const Category = ({ title, image, altText }) => {
-  const [data, setData] = useState([]);
+const Category = ({ image, altText, name }) => {
+  const [dataRestaurants, setDataRestaurants] = useState([]);
+  const [dataCategories, setDataCategories] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    (async () => {
-      const response = await fetch(
-        "http://frontendsourceryweb.s3-website.eu-central-1.amazonaws.com/restaurants.json"
-      );
-      const data = await response.json();
-      setData(data.restaurants);
-      setIsLoading(false);
-    })();
+    fetch(
+      "http://frontendsourceryweb.s3-website.eu-central-1.amazonaws.com/categories.json"
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw Error();
+        }
+        return res.json();
+      })
+      .then((dataCategories) => {
+        setDataCategories(dataCategories.categories);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    fetch(
+      "http://frontendsourceryweb.s3-website.eu-central-1.amazonaws.com/restaurants.json"
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw Error();
+        }
+        return res.json();
+      })
+      .then((dataRestaurants) => {
+        setDataRestaurants(dataRestaurants.restaurants);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
-  const sum = data
-    .map((item) =>
-      item.categories
-        .filter((item) => item === title)
-        .map((c) => (c = 1))
-        .reduce((a, b) => a + b, 0)
-    )
-    .reduce((a, b) => a + b, 0);
+  // const names = dataCategories.map(myFunc);
+  // function myFunc() {
+  //   if (name === "Donuts") {
+  //     name = "Sweets";
+  //   } else if (name === "Hot dogs") {
+  //     name = "Sandwich";
+  //   }
+  // }
+  // console.log(names);
 
   return (
-    <a className="section-btn" href="eat-out-next-page">
-      <div className="section-btn_item">
-        <h2 className="section-btn_title">
-          {title === "Breakfast" ? "Brunch" : title}
-        </h2>
-        <span className="section-btn_text">
-          {isLoading ? (
-            <div>Loading</div>
-          ) : (
-            <div>
-              {title === "Soups" ? sum + 1 : sum} {sum > 1 ? "places" : "place"}
+    <section className="section-container">
+      <h2 className="section-container_title">Categories</h2>
+      <Grid breakpointCols={[1, 2, 3, 4]}>
+        {dataCategories.map((name) => (
+          <a key={name} className="section-btn" href="eat-out-next-page">
+            <div className="section-btn_item">
+              <h2 className="section-btn_title">
+                {name === "Breakfast" ? "Brunch" : name}
+                <span className="section-btn_text">
+                  {isLoading ? (
+                    <div>Loading</div>
+                  ) : (
+                    <div>
+                      {dataRestaurants
+                        .map(
+                          (item) =>
+                            item.categories.filter(
+                              (item) =>
+                                item ===
+                                (name === "Brunch" ? "Breakfast" : name)
+                            ).length
+                        )
+                        .reduce((a, b) => a + b, 0) +
+                        " " +
+                        "places"}
+                      {/* {sum > 1 ? "places" : "place"} */}
+                    </div>
+                  )}
+                </span>
+              </h2>
             </div>
-          )}
-        </span>
-      </div>
-      <figure>
-        <img className="section-btn_img" src={image} alt={altText} />
-      </figure>
-    </a>
+
+            <figure>
+              <img className="section-btn_img" src={image} alt={altText} />
+            </figure>
+          </a>
+        ))}
+      </Grid>
+    </section>
   );
 };
 
 Category.propTypes = {
-  title: PropTypes.string,
-  number: PropTypes.string,
   image: PropTypes.string,
   altText: PropTypes.string,
+  name: PropTypes.string,
 };
 
 export default Category;
--- src/components/EatOutCategories/EatOutCategories.jsx
+++ src/components/EatOutCategories/EatOutCategories.jsx
@@ -1,82 +1,59 @@
 import React from "react";
 import Category from "./Category/Category";
-import Grid from "components/Grid/Grid";
-import ramen from "assets/ramen.svg";
-import pizza from "assets/pizza.svg";
-import pancakes from "assets/pancakes.svg";
-import salads from "assets/salads.svg";
-import sandwich from "assets/sandwich.svg";
-import burger from "assets/burger.svg";
-import sushi from "assets/sushi.svg";
-import soups from "assets/soups.svg";
-import kebab from "assets/kebab.svg";
-import brunch from "assets/brunch.svg";
-import sweets from "assets/sweets.svg";
-import grill from "assets/grill.svg";
+import ramen from "assets/RestaurantCategoriesIcons/ramen.svg";
+// import pizza from "assets/RestaurantCategoriesIcons/pizza.svg";
+// import pancakes from "assets/pancakes.svg";
+// import salads from "assets/salads.svg";
+// import sandwich from "assets/sandwich.svg";
+// import burger from "assets/burger.svg";
+// import sushi from "assets/sushi.svg";
+// import soups from "assets/soups.svg";
+// import kebab from "assets/kebab.svg";
+// import brunch from "assets/brunch.svg";
+// import sweets from "assets/sweets.svg";
+// import grill from "assets/grill.svg";
 import "./EatOutCategories.scss";
 
 const EatOutCategories = () => {
   return (
-    <section className="section-container">
-      <h2 className="section-container_title">Categories</h2>
-      <Grid breakpointCols={[1, 2, 3, 4]}>
+    <div>
+      <Category image={ramen} />
+      {/* <Category title="Pizza" number="" image={pizza} altText="Pizza slice" />
         <Category
-          title="Ramen"
-          number=""
-          image={ramen}
-          altText="Bowl of ramen"
-        />
-        <Category title="Pizza" number="" image={pizza} altText="Pizza slice" />
-        <Category
-          title="Pancakes"
           image={pancakes}
           altText="Pile of pancakes"
         />
         <Category
-          title="Salads"
-          number=""
           image={salads}
           altText="Plate of salads"
         />
         <Category
-          title="Sandwich"
-          number=""
           image={sandwich}
           altText="Sandwich"
         />
-        <Category title="Burger" number="" image={burger} altText="Burger" />
+        <Category image={burger} altText="Burger" />
         <Category
-          title="Sushi"
-          number=""
           image={sushi}
           altText="Four sushi rolls"
         />
         <Category
-          title="Soups"
-          number=""
           image={soups}
           altText="Bowl of soup"
         />
-        <Category title="Kebab" number="" image={kebab} altText="Kebab" />
+        <Category image={kebab} altText="Kebab" />
         <Category
-          title="Breakfast"
-          number=""
           image={brunch}
           altText="Two fried eggs"
         />
         <Category
-          title="Sweets"
           image={sweets}
           altText="Donut covered with chocolate"
         />
         <Category
-          title="Grill"
-          number=""
           image={grill}
           altText="Two grilled meat skewers"
-        />
-      </Grid>
-    </section>
+        /> */}
+    </div>
   );
 };
 
