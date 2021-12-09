fcab9e85b445cc9ac7b198a9bb99272f61bf89eb
martynas9 <3martynas@gmail.com>
2021-12-09 22:50:44 +0200

    created 2 new components and edited routing

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Layout from "components/Layout/Layout";
 import Dashboard from "pages/Dashboard/Dashboard";
 import BookReservations from "pages/BookReservations/BookReservations";
 import EatOutPage from "pages/EatOutPage/EatOutPage";
+import EatOutCategoryPage from "pages/EatOutCategoryPage/EatOutCategoryPage";
 import Reservations from "pages/Reservations/Reservations";
 import DeviceReservations from "pages/DeviceReservations/DeviceReservations";
 import NotFound from "pages/NotFound/NotFound";
 import Register from "pages/Register/Register";
 import Login from "pages/Login/Login";
 import { UserProvider } from "components/UserContext/UserContext";
+import EatOutRestaurantPage from "pages/EatOutRestaurantPage/EatOutRestaurantPage";
 
 function App() {
   return (
@@ -28,6 +30,14 @@ function App() {
               <Route path="devices" element={<DeviceReservations />} />
             </Route>
             <Route path="/eat-out" element={<EatOutPage />} />
+            <Route
+              path="/eat-out/restaurant/:restaurantId"
+              element={<EatOutRestaurantPage />}
+            />
+            <Route
+              path="/eat-out/:categoryName"
+              element={<EatOutCategoryPage />}
+            />
             <Route path="/404" element={<NotFound />} />
             <Route path="*" element={<Navigate replace to="/404" />} />
           </Route>
