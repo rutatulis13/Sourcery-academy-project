1fde1d72e32d852bd2dc9aa6e828a599bb34ae74
godagin <goda.gintalaite@gmail.com>
2022-01-19 03:32:47 +0200

    fix pagination of less than 5 pages

--- src/components/ItemList/Pagination/Pagination.jsx
+++ src/components/ItemList/Pagination/Pagination.jsx
@@ -11,84 +11,134 @@ const Pagination = ({
   const totalPages = Math.ceil(totalItems / itemsPerPage);
 
   return (
-    <nav className="pagination-block">
-      {currentPage > 1 && (
-        <a
-          className="pagination-block__page-button"
-          href="#list"
-          onClick={() => handlePageChange(currentPage - 1)}
-        >
-          {"<"}
-        </a>
+    <>
+      {totalPages > 1 && totalPages !== 3 && totalPages !== 4 && (
+        <nav className="pagination-block">
+          {currentPage > 1 && totalPages > 4 && (
+            <a
+              className="pagination-block__page-button"
+              href="#list"
+              onClick={() => handlePageChange(currentPage - 1)}
+            >
+              {"<"}
+            </a>
+          )}
+
+          {currentPage !== 1 && (
+            <a
+              className="pagination-block__page-button"
+              href="#list"
+              onClick={() => handlePageChange(1)}
+            >
+              1
+            </a>
+          )}
+
+          {currentPage > 2 && totalPages > 4 && (
+            <button className="pagination-block__page-button">{"..."}</button>
+          )}
+
+          {currentPage === totalPages && totalPages > 4 && (
+            <a
+              className="pagination-block__page-button"
+              href="#list"
+              onClick={() => handlePageChange(currentPage - 1)}
+            >
+              {currentPage - 1}
+            </a>
+          )}
+
+          <a
+            className="pagination-block__page-button pagination-block__page-button--current"
+            href="#list"
+            onClick={() => handlePageChange(currentPage)}
+          >
+            {currentPage}
+          </a>
+          {currentPage === 1 && totalPages > 4 && (
+            <a
+              className="pagination-block__page-button"
+              href="#list"
+              onClick={() => handlePageChange(currentPage + 1)}
+            >
+              {currentPage + 1}
+            </a>
+          )}
+          {currentPage < totalPages - 1 && totalPages > 4 && (
+            <button className="pagination-block__page-button">{"..."}</button>
+          )}
+
+          {currentPage !== totalPages && (
+            <a
+              className="pagination-block__page-button"
+              href="#list"
+              onClick={() => handlePageChange(totalPages)}
+            >
+              {totalPages}
+            </a>
+          )}
+          {currentPage < totalPages && totalPages > 4 && (
+            <a
+              className="pagination-block__page-button"
+              href="#list"
+              onClick={() => handlePageChange(currentPage + 1)}
+            >
+              {">"}
+            </a>
+          )}
+        </nav>
       )}
-      {currentPage === 1 ? (
-        <a
-          className="pagination-block__page-button pagination-block__page-button--current"
-          href="#list"
-          onClick={() => handlePageChange(1)}
-        >
-          1
-        </a>
-      ) : (
-        <a
-          className="pagination-block__page-button"
-          href="#list"
-          onClick={() => handlePageChange(1)}
-        >
-          1
-        </a>
+      {totalPages >= 3 && totalPages <= 4 && (
+        <nav className="pagination-block">
+          <a
+            className={
+              currentPage === 1
+                ? "pagination-block__page-button pagination-block__page-button--current"
+                : "pagination-block__page-button"
+            }
+            href="#list"
+            onClick={() => handlePageChange(1)}
+          >
+            1
+          </a>
+          <a
+            className={
+              currentPage === 2
+                ? "pagination-block__page-button pagination-block__page-button--current"
+                : "pagination-block__page-button"
+            }
+            href="#list"
+            onClick={() => handlePageChange(2)}
+          >
+            2
+          </a>
+          <a
+            className={
+              currentPage === 3
+                ? "pagination-block__page-button pagination-block__page-button--current"
+                : "pagination-block__page-button"
+            }
+            href="#list"
+            onClick={() => handlePageChange(3)}
+          >
+            3
+          </a>
+          {totalPages === 4 && (
+            <a
+              className={
+                currentPage === 4
+                  ? "pagination-block__page-button pagination-block__page-button--current"
+                  : "pagination-block__page-button"
+              }
+              href="#list"
+              onClick={() => handlePageChange(4)}
+            >
+              4
+            </a>
+          )}
+        </nav>
       )}
-      {currentPage !== 2 && currentPage !== 1 && (
-        <button className="pagination-block__page-button">{"..."}</button>
-      )}
-      {currentPage === totalPages && (
-        <a
-          className="pagination-block__page-button"
-          href="#list"
-          onClick={() => handlePageChange(currentPage - 1)}
-        >
-          {currentPage - 1}
-        </a>
-      )}
-      {currentPage !== 1 ? (
-        <a
-          className="pagination-block__page-button pagination-block__page-button--current"
-          href="#list"
-          onClick={() => handlePageChange(currentPage)}
-        >
-          {currentPage}
-        </a>
-      ) : (
-        <a
-          className="pagination-block__page-button"
-          href="#list"
-          onClick={() => handlePageChange(currentPage + 1)}
-        >
-          {currentPage + 1}
-        </a>
-      )}
-      {currentPage !== totalPages - 1 && currentPage !== totalPages && (
-        <button className="pagination-block__page-button">{"..."}</button>
-      )}
-      {currentPage !== totalPages && (
-        <a
-          className="pagination-block__page-button"
-          href="#list"
-          onClick={() => handlePageChange(totalPages)}
-        >
-          {totalPages}
-        </a>
-      )}
-      {currentPage < totalPages && (
-        <a
-          className="pagination-block__page-button"
-          href="#list"
-          onClick={() => handlePageChange(currentPage + 1)}
-        >
-          {">"}
-        </a>
-      )}
-    </nav>
+    </>
   );
 };
 Pagination.propTypes = {
@@ -98,3 +148,35 @@ Pagination.propTypes = {
   handlePageChange: PropTypes.func,
 };
 export default Pagination;
+
+/*
+{currentPage === totalPages && totalPages !== 2 && (
+            <a
+              className="pagination-block__page-button"
+              href="#list"
+              onClick={() => handlePageChange(currentPage - 1)}
+            >
+              {currentPage - 1}
+            </a>
+          )}
+
+          {totalPages === 4 && currentPage !== 4 && (
+            <a
+              className="pagination-block__page-button"
+              href="#list"
+              onClick={() => handlePageChange(currentPage - 1)}
+            >
+              {totalPages - 1}
+            </a>
+          )}
+
+          {currentPage !== totalPages && totalPages !== 2 && (
+            <a
+              className="pagination-block__page-button"
+              href="#list"
+              onClick={() => handlePageChange(totalPages)}
+            >
+              {totalPages}
+            </a>
+          )}
+*/
